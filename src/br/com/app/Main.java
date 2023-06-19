5561026ed0527f5f2821ab720019fe4903b5931c
João Pedro G. C <115590969+jpgercc@users.noreply.github.com>
2023-06-19 06:06:02 -0300

    Add files via upload

---
+++ src/br/com/app/Main.java
@@ -0,0 +1,10 @@
+package br.com.app;
+
+import br.com.app.controller.AppController;
+
+public class Main {
+    public static void main(String[] args) {
+        AppController appController = new AppController();
+        appController.startApp();
+    }
+}
